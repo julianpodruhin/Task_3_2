7906352c36ec0dbdfb298cbd41a1eb24e43b794d
yulianpodruhin <yulianpodruhin@mail.ru>
2023-02-27 14:08:50 +0300

    Lab 3 ex 2

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Car {
+import java.awt.*;
+
+public class Car
+{
+   private String model ;
+   private int weight;
+   private Color color;
+
+   public Car (Color color)
+   {
+      this.color = color;
+
+   }
+   public Car(String model, int weight)
+   {
+      this.model = model;
+      this.weight = weight;
+   }
+   public Car()
+   {
+      System.out.println("Машина произведена");
+   }
+
+
+   public String getModel() {
+      return model;
+   }
+
+   public void setModel(String model) {
+      this.model = model;
+   }
+
+   public int getWeight() {
+      return weight;
+   }
+
+   public void setWeight(int weight) {
+      weight = 800;
+      this.weight = weight;
+   }
+
+   public Color getColor() {
+      return color;
+   }
+
+   public void setColor(Color color) {
+      this.color = color;
+   }
+
+   @Override
+   public String toString() {
+      return "Car{" +
+              "model='" + model + '\'' +
+              ", weight=" + weight +
+              ", color=" + color +
+              '}';
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.awt.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Car car_1 = new Car();
+        car_1.setModel("Ford");
+        car_1.setColor(Color.red);
+        car_1.setWeight(900);
+        System.out.println(car_1);
+        Car сar_2 = new Car();
+        сar_2.setModel("Mazda");
+        сar_2.setColor(Color.yellow);
+        сar_2.setWeight(1100);
+        System.out.println(сar_2);
     }
 }
